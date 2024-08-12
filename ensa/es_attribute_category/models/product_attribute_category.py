8ba7d67be9df7a8dd3693bd491279e24a4f7ae79
Samet Yardım <smt.yrdm@gmail.com>
2024-08-12 11:53:51 +0300

    Rewrited product_attribute_action

--- ensa/es_attribute_category/models/product_attribute_category.py
+++ ensa/es_attribute_category/models/product_attribute_category.py
@@ -1,5 +1,6 @@
 from odoo import api, fields, models, _
 
+
 class ProductAttributeCategory(models.Model):
     _name = 'product.attribute.category'
     _description = 'Product Attribute Category'
@@ -7,8 +8,20 @@ class ProductAttributeCategory(models.Model):
     name = fields.Char(string='Category Name', required=True)
     product_attributes = fields.One2many('product.attribute', 'attribute_category_id', string='Product Attributes')
 
+    def product_attribute_action(self):
+        self.ensure_one()
+        return {
+            'type': 'ir.actions.act_window',
+            'name': 'Product Attributes',
+            'res_model': 'product.attribute',
+            'view_mode': 'tree,form',
+            'domain': [('attribute_category_id', '=', self.id)],
+            'context': {'default_attribute_category_id': self.id},
+        }
+
 
 class ProductAttribute(models.Model):
     _inherit = 'product.attribute'
 
-    attribute_category_id = fields.Many2one('product.attribute.category', string='Attribute Category', index=True, required=False)
+    attribute_category_id = fields.Many2one('product.attribute.category', string='Attribute Category', index=True,
+                                            required=False)
