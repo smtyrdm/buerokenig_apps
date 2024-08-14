0e26719e150682523be7b06eb4eff2b8a5192e11
Samet Yardım <smt.yrdm@gmail.com>
2024-08-14 10:10:51 +0300

    Magento 2 Rest Api Get Product Type Added

--- magento2/magento2/wizard/product/fetch_products.py
+++ magento2/magento2/wizard/product/fetch_products.py
@@ -9,35 +9,16 @@ class ProductFetchWizard(models.Model):
     _name = 'product.fetch.wizard'
     _description = 'Product Fetch Wizard'
 
-    # def fetch_products(self):
-    #     """Fetch products from Magento"""
-    #     url = '/rest/all/V1/products?searchCriteria%5BpageSize%5D=5&searchCriteria%5BcurrentPage%5D=1'
-    #     type = 'GET'
-    #     magento_products = self.env['magento.connector'].magento_api_call(headers={}, url=url, type=type)
-    #     try:
-    #         items = magento_products.get('items', [])
-    #         for i in items:
-    #             products = self.env['magento.product'].search([('sku', '=', i['sku'])])
-    #             values = {
-    #                 'sku': i['sku'],
-    #                 'name': i['name'],
-    #                 'price': i.get('price', 0.0),
-    #                 'status': str(i.get('status', 0)),
-    #                 'type': i.get('type_id'),
-    #                 'visibility': str(i.get('visibility')),
-    #             }
-    #             if products:
-    #                 products.sudo().write(values)
-    #             else:
-    #                 self.env['magento.product'].sudo().create(values)
-    #
-    #     except Exception as e:
-    #         _logger.info("Exception occurred %s", e)
-    #         raise exceptions.UserError(_("Error Occurred %s") % e)
+    product_type = fields.Selection([
+        ('simple', 'Simple Products'),
+        ('configurable', 'Configurable Products'),
+    ], string="Product Type", required=True, default='simple')
 
     def fetch_products(self):
         """Fetch products from Magento"""
-        url = '/rest/all/V1/products?searchCriteria%5BpageSize%5D=5&searchCriteria%5BcurrentPage%5D=1'
+        product_type = self.product_type
+        search_criteria = f'searchCriteria[filterGroups][0][filters][0][field]=type_id&searchCriteria[filterGroups][0][filters][0][value]={product_type}'
+        url = f'/rest/all/V1/products?{search_criteria}&searchCriteria%5BpageSize%5D=5&searchCriteria%5BcurrentPage%5D=1'
         magento_products = self._magento_api_call(url)
 
         if not magento_products:
@@ -58,15 +39,22 @@ class ProductFetchWizard(models.Model):
     def _process_product(self, item):
         """Process each product item from Magento"""
         dropship_route = self.env['stock.location.route'].search([('name', '=', 'Dropship')], limit=1)
+        buy_route = self.env['stock.location.route'].search([('name', '=', 'Buy')], limit=1)
+        vendor = self.env['res.partner'].search([('name', '=', 'Tischkönig GmbH'),('route_ids', 'in', buy_route.ids)], limit=1)
         product_template = self.env['product.template'].search([('default_code', '=', item['sku'])])
+        existing_seller = product_template.seller_ids.filtered(lambda s: s.name.id == vendor.id)
         values = {
             'name': item['name'],
             'default_code': item['sku'],
             'list_price': item.get('price', 0.0),
             'type': 'product',
             'magento': True,
-            'route_ids': [(6, 0, [dropship_route.id])]  # Dropship rotasını ekle
+            'route_ids': [(6, 0, [dropship_route.id])] , # Dropship rotasını ekle
         }
+
+        # Eğer mevcut bir satıcı yoksa, yeni satıcı ekle
+        if not existing_seller:
+            values['seller_ids'] = [(0, 0, {'name': vendor.id})]  # Vendor id'sini ekle
 
         if product_template:
             product_template.sudo().write(values)
