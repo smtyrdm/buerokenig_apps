b2f867e6cc403fa601795861acba46ff5c67632a
Samet Yardım <smt.yrdm@gmail.com>
2024-09-13 15:09:23 +0300

    Rewrited Magento Product Options for magento_sku

---
+++ magento2/magento2_product/models/magento_product_option.py
@@ -0,0 +1,214 @@
+from odoo import models, fields, api
+import json
+import logging
+import ast
+_logger = logging.getLogger(__name__)
+
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    magento_json_data = fields.Text(string='JSON Data')
+
+    def action_magento_option(self):
+        for rec in self:
+            if rec.magento_json_data:
+                # JSON verisini yükle
+                data = json.loads(rec.magento_json_data or "[]")
+                unmatched_titles = set()
+
+                for item in data:
+                    title = item.get('title')
+                    if title:
+                        matched = False
+                        for ptal in rec.attribute_line_ids:
+                            if (
+                                    (ptal.label and ptal.label == title) or
+                                    (ptal.label and ptal.label in title) or
+                                    (ptal.label and title in ptal.label) or
+                                    ptal.attribute_id.name == title or
+                                    ptal.attribute_id.name in title or
+                                    title in ptal.attribute_id.name
+                            ):
+                                ptal.magento_option = title
+                                matched = True
+                                self.match_values_with_attribute_line(ptal, item.get('values', []))
+                                break
+                            elif ptal.magento_option == title:
+                                matched = True
+                                self.match_values_with_attribute_line(ptal, item.get('values', []))
+                                break
+
+                        if not matched:
+                            check_skus = item.get('values', [])
+                            sku_check_results = ['sku' in sku for sku in check_skus]
+                            if any(sku_check_results):
+                                unmatched_titles.add(title)
+
+                # Eşleşmeyen title'lar için loglama yap
+                for unmatched_title in unmatched_titles:
+                    _logger.warning("Title'lar eşleşmiyor lütfen kontrol ediniz: %s %s", unmatched_title, rec.name)
+
+    def match_values_with_attribute_line(self, ptal, values):
+        for value in values:
+            value_title = value.get('title', '')
+            value_sku = value.get('sku', '')
+            value_price = value.get('price',0)
+            for rec in ptal.value_ids:
+                if (
+                        ptal.magento_option and
+                        isinstance(rec.name, str) and rec.name == value_title or
+                        isinstance(rec.variant_suffix, str) and rec.variant_suffix == value_sku or
+                        (isinstance(rec.variant_suffix, str) and isinstance(value_sku,str) and rec.variant_suffix in value_sku) or
+                        (isinstance(value_sku, str) and isinstance(rec.variant_suffix,str) and value_sku in rec.variant_suffix)
+                ):
+                    ptav = self.env['product.template.attribute.value'].search([('product_attribute_value_id', '=', rec.id),('attribute_line_id', '=', ptal.id)],limit=1)
+                    ptav.price_extra = value_price
+                    break
+
+
+    # compute_dynmaic_fields = fields.Boolean(string='Compute Dynmaic Fields', compute='_compute_dynmaic_fields')
+    #
+    # def _compute_dynmaic_fields(self):
+    #     if self.attribute_line_ids:
+    #         self.compute_dynmaic_fields = True
+    #         for rec in self.attribute_line_ids:
+    #             rec.magento_attribute_titles(self.id)
+    #     else:
+    #         self.compute_dynmaic_fields = False
+    #
+    # def update_attribute_values_from_magento(self):
+    #
+    #     try:
+    #         magento_data = json.loads(self.magento_json_data)
+    #     except json.JSONDecodeError as e:
+    #         raise ValueError(f"JSON formatında hata: {e}")
+    #     for data in magento_data:
+    #         for value in data.get('values', {}):
+    #             sku = value.get('sku')
+    #             price_extra = value.get('price')
+    #             name = value.get('title')
+    #
+    #             if sku and price_extra:
+    #                 attribute_value = self.env['product.template.attribute.value'].search([
+    #                     ('product_tmpl_id', '=', self.id),
+    #                     '|',
+    #                     ('product_attribute_value_id.variant_suffix', '=', sku),
+    #                     ('product_attribute_value_id.name', '=', name)
+    #                 ], limit=1)
+    #
+    #                 if attribute_value:
+    #                     attribute_value.write({'price_extra': price_extra})
+    #                 else:
+    #                     print(f"SKU: {sku}")
+
+
+class ProductTemplateAttributeLine(models.Model):
+    _inherit = 'product.template.attribute.line'
+
+    magento_option = fields.Char(string="Magento Option")
+
+    @api.model
+    def dynamic_magento_option(self):
+        # <field name="magento_option" widget="dynamic_dropdown" options="{'values':'_dynamic_magento_option'}" context="{'product_tmpl_id': product_tmpl_id}"/>
+        values = []
+        product_tmpl_id = self.env.context.get('product_tmpl_id', '')
+        if product_tmpl_id:
+            product_tmpl = self.env['product.template'].browse(product_tmpl_id)
+            data = json.loads(product_tmpl.magento_json_data or "[]")
+            for option in data:
+                values += [
+                    (f"{option.get('title', '')}", f"{option.get('title', '')}"),
+                ]
+        return values
+
+    def write(self, vals):
+        # burda yapılan product.template çok beklemesini engelliyor.
+        if 'magento_option' in vals and len(vals) == 1:
+            query = """
+                UPDATE product_template_attribute_line
+                SET magento_option = %s
+                WHERE id = %s
+            """
+            if not vals['magento_option']:
+                vals['magento_option'] = ''
+            self.env.cr.execute(query, (vals['magento_option'], self.id))
+            self.invalidate_cache(['magento_option'])
+            return True
+        return super(ProductTemplateAttributeLine, self).write(vals)
+
+    def action_open_ptal_magento(self):
+        return {
+            'type': 'ir.actions.act_window',
+            'name': 'PTAL Related Model',
+            'view_mode': 'form',
+            'res_model': 'product.template.attribute.line',
+            'res_id': self.id,
+            'target': 'new',  # çünkü veri çoksa sayfa baya bekliyor.
+        }
+
+
+class ProductTemplateAttributeValue(models.Model):
+    _inherit = 'product.template.attribute.value'
+
+    magento_option_value = fields.Char(string="Magento Option")
+
+    @api.model
+    def dynamic_magento_option_value(self):
+        # <field name="magento_option_value" widget="dynamic_dropdown" options="{'values':'dynamic_magento_option'}" context="{'product_tmpl_id': product_tmpl_id}"/>
+        values = []
+        attribute_line_id = self.env.context.get('attribute_line_id', '')
+        if attribute_line_id:
+            attribute_line = self.env['product.template.attribute.line'].browse(attribute_line_id)
+            magento_option = attribute_line.magento_option
+            data = json.loads(attribute_line.product_tmpl_id.magento_json_data or "[]")
+            for item in data:
+                if item.get("title") == magento_option:
+                    for value in item.get('values', []):
+                        #values += [(value.get('price', 0), f"{value.get('title', '')} SKU: {value.get('sku', '')}")]
+                        val = {"title": value.get('title', ""), "price": value.get('price', 0), "sku": value.get('sku', "")}
+                        values += [
+                            (f'{val}', f'{val}'),
+                        ]
+        return values
+
+    def write(self, vals):
+        if 'magento_option_value' in vals:
+            vals['price_extra'] = ast.literal_eval(vals['magento_option_value'] or "{}").get('price',0)
+        return super(ProductTemplateAttributeValue, self).write(vals)
+
+    @api.model
+    def create(self, vals):
+        if 'magento_option_value' in vals:
+            vals['price_extra'] = ast.literal_eval(vals['magento_option_value'] or "{}").get('price',0)
+        return super(ProductTemplateAttributeValue, self).create(vals)
+
+#
+#
+#     @api.model
+#     def magento_attribute_titles(self,id = ""):
+#         if id:
+#             product_template = self.env['product.template'].browse(id)
+#             if not product_template.magento_json_data:
+#                 return []
+#             try:
+#                 json_data = json.loads(product_template.magento_json_data)
+#             except json.JSONDecodeError:
+#                 return []
+#
+#             titles = set()
+#             for item in json_data:
+#                 titles.add(item.get('title', ''))
+#
+#             return [(title, title) for title in titles]
+#         else:
+#             return []
+#
+#     magento_attribute_title = fields.Selection(selection='magento_attribute_titles',
+#                                                string='Magento Attribute Title')
+
+# class ProductCategory(models.Model):
+#     _inherit = 'product.category'
+#
+#     magento_name = fields.Char(string="Magento Name")
+#     magento_id = fields.Char(string="Magento ID")
---
+++ magento2/magento2_product/wizard/fetch_products_option.py
@@ -0,0 +1,92 @@
+import logging
+
+from odoo import models, fields, exceptions, _
+import urllib.parse
+import base64
+import requests
+import json
+
+_logger = logging.getLogger(__name__)
+
+
+class ProductFetchWizard(models.Model):
+    _inherit = 'product.fetch.wizard'
+
+    product_type = fields.Selection(
+        selection_add=[('product_option_price_update', 'Product Option Price Update')]  # Yeni seçenek ekleme
+    )
+
+
+    def fetch_products(self):
+        # Orijinal işlevi genişletme
+        super(ProductFetchWizard, self).fetch_products()
+        if self.product_type == 'product_option_price_update':
+            self.fetch_product_option()
+
+    def fetch_product_option(self):
+        # products = self.env['product.template'].search([('magento', '=', True), ('variant_code', '=', 'SSP-EUROT-2-SZ')])
+        # products = self.env['product.template'].search([('magento', '=', True), ('variant_code', '=', 'SSP-EUROT-2-SZ')])
+        products = self.env['product.template'].search([('magento', '=', True)])
+        for product in products:
+            sku = urllib.parse.quote(product.magento_sku)
+            url = f'/rest/all/V1/products/{sku}'
+            prod_option = self._magento_api_call(url)
+            if not prod_option:
+                continue
+
+            product_type = prod_option.get('type_id')
+
+            # conf_skus alanı için verileri hazırlayın
+            conf_skus = False
+            if product_type == 'configurable':
+                # confs = prod_option.get('extension_attributes', {}).get('configurable_product_options',[])
+                # for conf in confs:
+                #     attribute_id = conf.get('attribute_id')
+                #     self.configurable_product_options(attribute_id)
+                conf_skus = self.conf_product_price(sku)
+
+            options = prod_option.get('options', [])
+
+            if conf_skus:
+                options.append({
+                    'title': 'CONFS', # title eklenecek
+                    'values': conf_skus
+                })
+
+            try:
+                json_data = json.dumps(options)
+                product.magento_json_data = json_data
+            except (TypeError, ValueError) as e:
+                raise ValueError(f"JSON formatında hata: {e}")
+
+    def configurable_product_options(self, attribute_id):
+        # url içerisindeki all verisi ile sku'lar alınıyor. Eğer "all" yerine store code gönderilirse dile göre çevirileri geliyor.
+        url = f'/rest/all/V1/products/attributes/{attribute_id}'
+        # url = f'/rest/de/V1/products/attributes/{attribute_id}'
+        # url = f'/rest/fr/V1/products/attributes/{attribute_id}'
+        json_data = self._magento_api_call(url) or []
+        options = json_data.get('options', [])
+        _logger.warning('Options %s', options)
+
+
+
+    def conf_product_price(self, sku):
+        url = f'/rest/V1/configurable-products/{sku}/children'
+        children_data = self._magento_api_call(url) or []
+        conf_skus = []
+        sku_price_list = [{'sku': child.get('sku'), 'price': child.get('price')} for child in children_data if
+                          'sku' in child and 'price' in child]
+
+        if sku_price_list:
+            min_price = min(sku['price'] for sku in sku_price_list)
+
+            conf_skus = [{
+                'sku': sku['sku'].split('-')[-1],
+                'price': round(sku['price'] - min_price, 2)
+            } for sku in sku_price_list]
+
+        return conf_skus
+
+    def fetch_option_name_and_translate(self):
+
+        pass
